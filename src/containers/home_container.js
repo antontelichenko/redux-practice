84c57a9392777c95dbe6ab7915c8b1b18ee32842
geka412315 <geka412315@gmail.com>
2018-06-28 11:51:51 +0300

    react+redux simple music app

---
+++ src/containers/home_container.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import {connect} from 'react-redux';
+import { artistsListAll, artistList } from '../actions';
+import {bindActionCreators} from 'redux';
+
+import Search from '../components/search';
+import Artistlist from '../components/artistlist';
+
+class HomeContainer extends Component {
+
+    componentWillMount() {
+        this.props.artistsListAll();
+    }
+
+    getKeywords = (event) => {
+        let key = event.target.value;
+
+        this.props.artistList(key)
+    }
+
+    render(){
+        console.log(this.props)
+
+        return (
+            <div>
+                <Search keywords={this.getKeywords}/>
+                <Artistlist artists={this.props.artists.artistList}/>
+            </div>
+        )
+    }
+
+}
+
+function mapStateToProps(state){
+    return{
+        artists:state.artists
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({artistsListAll, artistList},dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer)
